4ff18ab86c1ae3e5274104692e62b0d2fea9ad0f
SpectralGT <atharvsingh1396@gmail.com>
2025-03-11 18:53:16 +0000

    coded a basic scene

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,36 @@
 import * as THREE from 'three';
+import { GLTFLoader } from 'three/examples/jsm/Addons.js';
 
-const camera = new THREE.PerspectiveCamera()
-console.log(THREE)
+const camera = new THREE.PerspectiveCamera(
+  10,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+)
+
+camera.position.z = 13;
+
+const scene = new THREE.Scene()
+let car;
+const loader = new GLTFLoader();
+loader.load('/model.glb',
+  function (gltf) {
+    car = gltf.scene;
+    console.log(car)
+    scene.add(car)
+    camera.lookAt(car.position)
+  },
+  function (xhr) {},
+  function (error) {}
+)
+
+
+const renderer = new THREE.WebGLRenderer({alpha:true});
+renderer.setSize(window.innerWidth,window.innerHeight)
+document.getElementById('container3D')?.append(renderer.domElement);
+
+const rerender3D = () =>{
+  requestAnimationFrame(rerender3D);
+  renderer.render(scene,camera)
+}
+rerender3D
